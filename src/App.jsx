7f08ab73d956339b5b5f8b4c1001e99d86f61033
Chaitanya <sdcbbarewar15@gmail.com>
2025-05-17 09:08:57 +0530

    Basic Layout is Added for the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import { Layout,Typography,Space } from 'antd'
 import './App.css'
 
-function App() {
- 
+import {Navbar,Exchanges,CryptoDetails,Cryptocurrencies,News,HomePage} from './components'
 
+
+
+function App() {
   return (
-    <div>Hello World</div>
+    <div className='app'>
+      <div className="navbar">
+       <Navbar/>
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage/>}/>
+              <Route path="/exchanges" element={<Exchanges/>}/>
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+              <Route path='/crypto/:coinId' element={<CryptoDetails/>}/>
+              <Route path='/news' element={<News/>}/>
+            </Routes>
+          </div>
+        </Layout>
+
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
   )
 }
 
